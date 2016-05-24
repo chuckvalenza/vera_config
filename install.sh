3315b98c36beee48031d9dd2eda830796432090d
Chuck Valenza <fallencx@gmail.com>
2016-05-24 00:16:38 -0400

    Add install script

---
+++ install.sh
@@ -0,0 +1,19 @@
+# /bin/sh
+# vera++ lint configurator install script
+
+rc=~/.bashrc
+VERA_ROOT=/usr/lib/vera++/
+
+if [ ! -f "$rc" ]
+then
+    touch $rc
+fi
+
+exists=`grep "alias vera++" $rc | wc -l`
+
+if [ $exists -ne 1 ]
+then
+    echo "alias vera++='vera++ -p cv_lint -P max-line-length=80'" >> $rc
+fi
+
+sudo cp cv_lint $VERA_ROOT/profiles/
